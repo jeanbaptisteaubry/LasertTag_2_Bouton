55ba5e86ce7ce766c72da19cc1dbbaff545d9aec
jeanbaptisteaubry <jean-baptiste.aubry@ac-besancon.fr>
2023-08-18 09:18:04 +0200

    plus uml

--- src/ButtonDebug.h
+++ src/ButtonDebug.h
@@ -3,29 +3,25 @@
 #include "ButtonInterface.h"
 #include <Arduino.h>
 
- 
-
-
 class ButtonDebug : public ButtonInterface
 {
 private:
-  /* data */
-    
+    /* data */
 
-   
-    size_t tableSize; 
+    size_t tableSize;
     int currentSimulationIndex = 0;
+
 public:
-    struct SimulationEntry {
-        uint32_t time;  // Temps (en millis) pour ce changement
-        int etatPin;    // Etat du pin à ce moment
+    struct SimulationEntry
+    {
+        uint32_t time; // Temps (en millis) pour ce changement
+        int etatPin;   // Etat du pin à ce moment
     };
-     const SimulationEntry* simulationTable;
-    ButtonDebug(uint8_t _PIN, const SimulationEntry* table, size_t size);
-    ButtonDebug(uint8_t _PIN, bool modePullUp, const SimulationEntry* table, size_t size);
-  void MAJ() override; //Mise à jour de l'état du bouton, à faire chaque cycle de setup();
-    void AfficherDebug() override; //Mise à jour de l'état du bouton, à faire chaque cycle de setup();
-    
-    };
+    const SimulationEntry *simulationTable;
+    ButtonDebug(uint8_t _PIN, const SimulationEntry *table, size_t size);
+    ButtonDebug(uint8_t _PIN, bool modePullUp, const SimulationEntry *table, size_t size);
+    void MAJ() override;           // Mise à jour de l'état du bouton, à faire chaque cycle de setup();
+    void AfficherDebug() override; // Mise à jour de l'état du bouton, à faire chaque cycle de setup();
+};
 
 #endif
